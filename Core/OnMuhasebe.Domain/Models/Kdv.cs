831be71988fb40a52507d2f371f74e51aafe9ca1
Fahrettin Solak <fhrttnsolak96@gmail.com>
2023-12-14 21:01:36 +0300

    #1 Kdv Table Completed

---
+++ Core/OnMuhasebe.Domain/Models/Kdv.cs
@@ -0,0 +1,18 @@
+﻿using OnMuhasebe.Domain.Models.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMuhasebe.Domain.Models
+{
+    public class Kdv:BaseModel
+    {
+        public string KdvName { get; set; }
+
+        public decimal KdvRatio { get; set; }
+        public ICollection<Price> Prices { get; set; }
+    }
+}
